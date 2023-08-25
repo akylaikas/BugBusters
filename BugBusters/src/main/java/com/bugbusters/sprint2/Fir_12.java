91286795ddc89d1093f0c0ae173b1006ba49ef48
marianabai <mmmarianabai@gmail.com>
2023-08-25 00:08:10 -0500

    added something

--- BugBusters/src/main/java/com/bugbusters/sprint2/Fir_12.java
+++ BugBusters/src/main/java/com/bugbusters/sprint2/Fir_12.java
@@ -3,5 +3,6 @@ package com.bugbusters.sprint2;
 public class Fir_12 {
     public static void main(String[] args) {
         System.out.println("new code");
+        System.out.println("added something");
     }
 }
