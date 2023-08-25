6217977d9aa895f39c20de1984e7441421eb5937
marianabai <mmmarianabai@gmail.com>
2023-08-25 00:20:08 -0500

    added text new

--- BugBusters/src/main/java/com/bugbusters/sprint2/Fir_12.java
+++ BugBusters/src/main/java/com/bugbusters/sprint2/Fir_12.java
@@ -4,5 +4,6 @@ public class Fir_12 {
     public static void main(String[] args) {
         System.out.println("new code");
         System.out.println("added something");
+        System.out.println("new");
     }
 }
