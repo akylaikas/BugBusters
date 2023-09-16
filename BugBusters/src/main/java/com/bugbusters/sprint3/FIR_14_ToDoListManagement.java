3522deebbc48dcee4acd926c12b9d73277aba178
bekturyusupov <bekturyusupov@gmail.com>
2023-09-16 10:48:19 -0500

    Deleted old dev file and created new with appropriate name

---
+++ BugBusters/src/main/java/com/bugbusters/sprint3/FIR_14_ToDoListManagement.java
@@ -0,0 +1,33 @@
+package com.bugbusters.sprint3;
+
+public class FIR_14_ToDoListManagement {
+}
+
+class data_management{
+    private static int task_id=100;
+    private String task_name;
+    private String date;
+    private String task_description;
+
+
+
+
+
+    //Which variables?
+    //Date entered, user entered, list of tasks, is it completed or not(maybe MAP)
+    //task description
+
+
+    public void add(){
+
+    }
+
+    public void remove(){
+
+    }
+
+    public void view(){
+
+    }
+
+}
