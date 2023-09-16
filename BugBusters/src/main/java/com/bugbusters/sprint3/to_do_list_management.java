b0bf72755d598ba8cee2915346adf7be4b17feb6
bekturyusupov <bekturyusupov@gmail.com>
2023-09-16 09:08:52 -0500

    FIR-14-To Do List Management

---
+++ BugBusters/src/main/java/com/bugbusters/sprint3/to_do_list_management.java
@@ -0,0 +1,21 @@
+package com.bugbusters.sprint3;
+
+public class to_do_list_management {
+}
+
+class data_management{
+
+
+    public void add(){
+
+    }
+
+    public void remove(){
+
+    }
+
+    public void view(){
+
+    }
+
+}
